93a89ffc316fd7b08bfea33925b6a0fc19f804dc
Dagne Drake <dagne.drake@protonmail.com>
2026-05-07 14:06:42 -0700

    organized java files into src folder 050726

---
+++ src/BinaryTree.java
@@ -0,0 +1,17 @@
+/**
+ * Base binary tree class.
+ */
+public class BinaryTree
+{
+    protected Node root;
+
+    /**
+     * Check if the binary tree is empty.
+     *
+     * @return true if empty, false otherwise
+     */
+    public boolean isEmpty()
+    {
+        return root == null;
+    }
+}
